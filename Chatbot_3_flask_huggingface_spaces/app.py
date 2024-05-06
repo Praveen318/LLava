a84b7155c68cf8fd8033b426f2bce7fa3b3433ab
Praveen <praveenkrsingh0309@gmail.com>
2024-05-06 16:59:09 +0530

    Done

--- Chatbot_3_flask_huggingface_spaces/app.py
+++ Chatbot_3_flask_huggingface_spaces/app.py
@@ -155,7 +155,7 @@ def get_bot_response():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 # from pymongo import MongoClient
